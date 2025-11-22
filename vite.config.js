4844caa487733d357adc26a4edf29ac9b5def1a1
murphydanielsk172 <murphyskdaniel172@gmail.com>
2025-11-22 23:21:05 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Daniel_Portfolio_CV/",
   plugins: [react()],
-  base:"/Daniel_Portfolio_CV"
 });
