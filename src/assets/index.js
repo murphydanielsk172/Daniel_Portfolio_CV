b3f851256fd1c0e1dbc40187cae9c543f7a76bc4
Daniel Murphy <murphyskdaniel172@gmail.com>
2025-11-23 21:14:50 +0000

    Update profile image and cleanup old assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -32,7 +32,7 @@ import logo from "./logo.svg";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import profile from "./profile.svg";
+import profile from "./profile.png";
 
 export {
   mathwork,
