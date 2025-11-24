5363fe2067bd3ddd732c44a78189be934c43f838
murphydanielsk172 <murphyskdaniel172@gmail.com>
2025-11-24 18:53:53 +0000

    Revise project cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -41,7 +41,7 @@ const ProjectCard = ({ name, description, tags, image, source_code_link }) => {
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p key={tag.name} className={`text-[14px] ${tag.color}`}>
-              #{tag.name}
+              {tag.name}
             </p>
           ))}
         </div>
